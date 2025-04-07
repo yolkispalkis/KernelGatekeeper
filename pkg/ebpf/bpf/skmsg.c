8d6a7f666b465743f54072743875aa8dca71c480
Заббаров Карим Хадиевич <khzabb10@mts.ru>
2025-04-07 12:05:04 +0600

    Refactor skmsg.c to simplify socket cookie handling in kernelgatekeeper_skmsg function. Removed unnecessary call to bpf_get_socket_cookie and updated logging for message redirection outcomes. This enhances code clarity and maintains efficient socket operation management.

--- pkg/ebpf/bpf/skmsg.c
+++ pkg/ebpf/bpf/skmsg.c
@@ -4,33 +4,34 @@
 
 SEC("sk_msg")
 int kernelgatekeeper_skmsg(struct sk_msg_md *msg) {
-    __u32 sock_cookie = bpf_get_socket_cookie(msg->sk);
-     if (sock_cookie == 0) {
-         #ifdef DEBUG
-         bpf_printk("SKMSG: Failed to get socket cookie for msg->sk\n");
-         #endif
-         return SK_DROP;
-     }
+    /*
+     * We don't need (and cannot) call bpf_get_socket_cookie() here.
+     * The bpf_msg_redirect_map() helper, when used with a BPF_MAP_TYPE_SOCKHASH map,
+     * implicitly uses the cookie of the socket associated with 'msg' (msg->sk)
+     * as the key to find the peer socket in the map.
+     * The 'key' argument (3rd arg) is ignored in this case and should be 0.
+     */
 
     #ifdef DEBUG
-    // bpf_printk("SKMSG: Triggered on sock cookie %u, size %u\n", sock_cookie, msg->size);
+    // Retrieve the cookie only for logging if absolutely necessary (and if allowed in future kernels?)
+    // For now, rely on the implicit behavior.
+    // bpf_printk("SKMSG: Triggered on implicit socket cookie, size %u\n", msg->size);
     #endif
 
-    int ret = bpf_msg_redirect_map(msg, &proxy_sock_map, sock_cookie, BPF_F_INGRESS);
+    int ret = bpf_msg_redirect_map(msg, &proxy_sock_map, 0, BPF_F_INGRESS);
 
+    // Existing logging for the return code is fine
     if (ret == SK_DROP) {
         #ifdef DEBUG
-         bpf_printk("SKMSG: Dropped message for cookie %u, peer likely closed or map error: %d\n", sock_cookie, ret);
+         bpf_printk("SKMSG: Dropped message, peer likely closed or map error: %d\n", ret);
         #endif
     } else if (ret == SK_PASS) {
         #ifdef DEBUG
-         bpf_printk("SKMSG: Passed message (no redirect target?) for cookie %u: %d\n", sock_cookie, ret);
-        #endif
-    } else {
-        #ifdef DEBUG
-         bpf_printk("SKMSG: Unexpected return code from redirect_map for cookie %u: %d\n", sock_cookie, ret);
+         // Pass usually means the socket wasn't found in the map for redirection
+         bpf_printk("SKMSG: Passed message (no redirect target?) ret: %d\n", ret);
         #endif
     }
+    // Removed redundant else/debug printk from original code for unexpected codes
 
     return ret;
 }
