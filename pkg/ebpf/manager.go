692abe56f402dba3899e6624d8f53abe8f999197
Заббаров Карим Хадиевич <khzabb10@mts.ru>
2025-04-11 09:11:17 +0600

    Add BPF generation commands in manager.go for connect4, sockops, and getsockopt. Enhance compatibility with system headers by specifying additional include paths.

--- pkg/ebpf/manager.go
+++ pkg/ebpf/manager.go
@@ -1,5 +1,9 @@
 package ebpf
 
+//go:generate go run -tags linux github.com/cilium/ebpf/cmd/bpf2go -cc clang -target bpf -cflags "-O2 -g -Wall -Werror -DDEBUG -I./bpf -I/usr/include/bpf -I/usr/include -I/usr/include/x86_64-linux-gnu" bpf_connect4 ./bpf/connect4.c -- -I./bpf
+//go:generate go run -tags linux github.com/cilium/ebpf/cmd/bpf2go -cc clang -target bpf -cflags "-O2 -g -Wall -Werror -DDEBUG -I./bpf -I/usr/include/bpf -I/usr/include -I/usr/include/x86_64-linux-gnu" bpf_sockops ./bpf/sockops.c -- -I./bpf
+//go:generate go run -tags linux github.com/cilium/ebpf/cmd/bpf2go -cc clang -target bpf -cflags "-O2 -g -Wall -Werror -DDEBUG -I./bpf -I/usr/include/bpf -I/usr/include -I/usr/include/x86_64-linux-gnu" bpf_getsockopt ./bpf/getsockopt.c -- -I./bpf
+
 import (
 	"context"
 	"errors"
